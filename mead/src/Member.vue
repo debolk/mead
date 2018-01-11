76334b45810be5f99933cdaefce91b3d28c552e3
Jakob Buis <jakob@jakobbuis.nl>
2018-01-11 12:19:43 +0100

    separate mobile quick actions

--- mead/src/Member.vue
+++ mead/src/Member.vue
@@ -17,13 +17,27 @@
                 Geen mobiel nummer bekend
             </span>
         </td>
-        <td>
-            <a href="#" class="button is-small" @click.prevent="updateDetailedView">Alle informatie</a>
+        <td class="actions">
+            <button class="button is-small is-hidden-tablet" @click="email" :disabled="!emailPresent">
+                <i class="fa fa-fw fa-envelope-o"></i>
+            </button>
+            <button class="button is-small is-hidden-tablet" @click="call" :disabled="!member.mobile">
+                <i class="fa fa-fw fa-phone"></i>
+            </button>
+            <button class="button is-small is-hidden-tablet" href="#" @click.prevent="updateDetailedView">
+                <i class="fa fa-fw fa-address-card-o"></i>
+            </button>
+
+            <a class="button is-small is-hidden-mobile" href="#" @click.prevent="updateDetailedView">
+                Alle informatie
+            </a>
         </td>
     </tr>
 </template>
 
 <script>
+import { link } from './helpers/PhoneNumber';
+
 export default {
     props: ['member'],
 
@@ -41,12 +55,28 @@ export default {
         updateDetailedView() {
             this.$emit('more-info', this.member);
         },
+
+        call() {
+            window.location.href = link(this.member.mobile);
+        },
+
+        email() {
+            window.location.href = this.emailLink;
+        },
     },
 };
 </script>
 
 <style lang="scss" scoped>
 .missing {
+    color: #999;
+}
+.actions {
+    min-width: 9em;
+}
+
+button[disabled] {
+    border-color: #999;
     color: #999;
 }
 </style>
